6bebf3954554c6f0c0692a4c87d89075ad0698b5
Max Ribbans <ribbans.max@gmail.com>
2021-04-26 13:41:42 -0400

    Add working solution

---
+++ even-fibonacci/even-fibonacci.rb
@@ -0,0 +1,10 @@
+def sum_even_fib_until(stop = 4000000, sum = 2, list = [1, 2])
+  if list[-1] + list[-2] > stop
+    sum
+  else
+    next_fib = list[-1] + list[-2]
+    sum += next_fib if next_fib.even?
+    list << next_fib
+    sum_even_fib_until(stop, sum, list)
+  end
+end

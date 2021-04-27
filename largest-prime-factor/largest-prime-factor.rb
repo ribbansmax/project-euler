61aedea915b21d593ec5e5923dc1a3ceb16f68ed
Max Ribbans <ribbans.max@gmail.com>
2021-04-27 12:13:20 -0400

    Refactor for elegance

--- largest-prime-factor/largest-prime-factor.rb
+++ largest-prime-factor/largest-prime-factor.rb
@@ -6,24 +6,22 @@ class PrimeFactor
 
   def factor(n, factors = [], primes = [3])
     if n % primes[-1] == 0
-      i = primes[-1]
-      factors << i
-      n = n / i
-      while primes.any? { |prime| i % prime == 0 }
-        i += 2
-      end
-      primes << i
-      factor(n, factors, primes)
+      factors << primes[-1]
+      n = n / primes[-1]
+      factor(n, factors, increment_p(primes))
     elsif primes[-1] >= n
       factors[-1] || n
     else
-      i = primes[-1]
-      while primes.any? { |prime| i % prime == 0 }
-        i += 2
-      end
-      primes << i
-      factor(n, factors, primes)
+      factor(n, factors, increment_p(primes))
     end
+  end
+
+  def increment_p(primes)
+    i = primes[-1]
+    while primes.any? { |prime| i % prime == 0 }
+      i += 2
+    end
+    primes << i
   end
 end
 

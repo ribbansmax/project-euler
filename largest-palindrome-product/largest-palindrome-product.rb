f8d87d0391d967064dd22fde7c3daf5cc8d0a4bf
Max Ribbans <ribbans.max@gmail.com>
2021-04-28 10:24:04 -0400

    WIP

--- largest-palindrome-product/largest-palindrome-product.rb
+++ largest-palindrome-product/largest-palindrome-product.rb
@@ -1,6 +1,31 @@
 class PalindromeProduct
+  attr_reader :palindromes
+
+  def initialize()
+    @palindromes = []
+  end
+
   def palindrome?(n)
     n.to_s == n.to_s.reverse
+  end
+
+  def find_palindromes
+    i = 999
+    j = 999
+    while i > 100
+      while j > 100
+        p = i * j
+        @palindromes << p if palindrome?(p)
+        j = j - 1
+      end
+      i = i - 1
+      j == 999
+    end
+  end
+
+  def largest_palindrome
+    find_palindromes()
+    @palindromes.max
   end
 end
 

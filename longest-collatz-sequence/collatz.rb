54e1c5bc4a42afc1b673802795395a68cd11e8ae
Max Ribbans <ribbans.max@gmail.com>
2021-05-20 15:45:44 -0400

    Add max_length of Collatz method

--- longest-collatz-sequence/collatz.rb
+++ longest-collatz-sequence/collatz.rb
@@ -1,4 +1,18 @@
 class Collatz
+  def max_length(i)
+    (1..i).max_by do |j|
+      length(j)
+    end
+  end
+
+  def length(n, l = 1)
+    if n == 1
+      l
+    else
+      length(step(n), l + 1)
+    end
+  end
+
   def step(n)
     if n.even?
       n / 2
